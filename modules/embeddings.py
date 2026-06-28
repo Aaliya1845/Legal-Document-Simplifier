b17bb63eb9f0c87af7777437d3209081ac3a5014
Aaliya1845 <tasbiyaansari2207@gmail.com>
2026-06-28 12:57:33 +0530

    Create embeddings.py

---
+++ modules/embeddings.py
@@ -0,0 +1,19 @@
+"""
+Embeddings module
+"""
+
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
+
+
+def create_embeddings(chunks):
+
+    embeddings = model.encode(
+        chunks,
+        show_progress_bar=False
+    )
+
+    return embeddings
